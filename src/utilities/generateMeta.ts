16455784e6ba7903f66717ae3c798a00b9a2a49a
schuyberg <schuyler.lindberg@ubc.ca>
2025-04-06 01:41:50 -0700

    default og image

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -8,7 +8,7 @@ import { getServerSideURL } from './getURL'
 const getImageURL = (image?: Media | Config['db']['defaultIDType'] | null) => {
   const serverUrl = getServerSideURL()
 
-  let url = serverUrl + '/website-template-OG.webp'
+  let url = serverUrl + '/api/media/file/SRS-Logo-white-bg-1.png'
 
   if (image && typeof image === 'object' && 'url' in image) {
     const ogUrl = image.sizes?.og?.url
