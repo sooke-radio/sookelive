9ff4e7719f437891daea4af4101ab0e438359fb4
schuyberg <schuyler.lindberg@ubc.ca>
2025-04-23 00:20:24 -0700

    max 7 in header

--- src/Header/config.ts
+++ src/Header/config.ts
@@ -17,7 +17,7 @@ export const Header: GlobalConfig = {
           appearances: false,
         }),
       ],
-      maxRows: 6,
+      maxRows: 7,
       admin: {
         initCollapsed: true,
         components: {
