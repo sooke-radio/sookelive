e827e10c0fd49395d17eb49c5c75ce19ee2218b6
schuyberg <schuyler.lindberg@ubc.ca>
2025-12-14 00:07:13 -0800

    update NextJS, fix type error

--- src/app/(frontend)/next/preview/route.ts
+++ src/app/(frontend)/next/preview/route.ts
@@ -1,20 +1,13 @@
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { NextRequest } from 'next/server'
 import { getPayload, type PayloadRequest } from 'payload'
 import configPromise from '@payload-config'
 import { CollectionSlug } from 'payload'
 
 const payloadToken = 'payload-token'
 
-export async function GET(
-  req: Request & {
-    cookies: {
-      get: (name: string) => {
-        value: string
-      }
-    }
-  },
-): Promise<Response> {
+export async function GET(req: NextRequest): Promise<Response> {
   const payload = await getPayload({ config: configPromise })
   const token = req.cookies.get(payloadToken)?.value
   const { searchParams } = new URL(req.url)
